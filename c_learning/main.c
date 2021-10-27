bf74e7ffaf285ebae584e8f6e2c97d323e073313
wanghang697 <wanghang20031106@163.com>
2021-10-27 18:59:34 +0800

    Initial Commit

---
+++ c_learning/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c_learning
+//
+//  Created by 汪航 on 2021/10/27.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
